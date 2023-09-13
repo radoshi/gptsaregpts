8953a9110472b15412fbc9b8135177980dc6127f
Rushabh Doshi <radoshi@gmail.com>
2023-09-13 09:22:57 -0700

    feat: alpha, beta, zeta calculations complete.

--- gptsaregpts/alter_db.py
+++ gptsaregpts/alter_db.py
@@ -3,18 +3,34 @@ import sqlite3
 import click
 
 
+def add_column(
+    cursor: sqlite3.Cursor, table_name: str, column_name: str, data_type: str
+) -> None:
+    check_sql = f"""
+    PRAGMA table_info({table_name});
+    """
+    columns = [column[1] for column in cursor.execute(check_sql).fetchall()]
+    if column_name in columns:
+        print(f"{column_name} column already exists")
+        return
+
+    alter_sql = f"""
+    ALTER TABLE {table_name} ADD COLUMN {column_name} {data_type};
+    """
+    cursor.executescript(alter_sql)
+    print(f"Successfully added {column_name} column.")
+
+
 @click.command()
 @click.option("--db-name", default="db.sqlite", help="Name of the SQLite database")
 def alter_db(db_name):
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect("db.sqlite")
     cursor = conn.cursor()
 
-    alter_sql = """
-    ALTER TABLE dwa_reference ADD COLUMN classification TEXT;
-    """
-
-    cursor.executescript(alter_sql)
-    print("Successfully alterned table")
+    add_column(cursor, "dwa_reference", "classification", "TEXT")
+    for col in ["alpha", "beta", "zeta"]:
+        add_column(cursor, "task_statements", col, "REAL")
+        add_column(cursor, "occupation_data", col, "REAL")
 
     # Commit changes and close connection
     conn.commit()
